24942c6b4e829114c0820d99a5d8d7245d95cc68
Fidele Rabearimanana <ramanantsoafidel@gmail.com>
2026-01-13 17:00:35 +0300

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppSidebar from './AppSidebar.vue'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import AppSidebarGroup from './AppSidebarGroup.vue';
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebarGroup />
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <slot />
+    </main>
+  </SidebarProvider>
+</template>
